205f1247e0e8a0370c700b8876d76dcb4bddf5e5
rockjenya <yevheniizaiats@gmail.com>
2020-06-23 22:05:34 +0300

    Add a 7.7 and 7.8 practice task

--- Rozdil 7/along_part7.c
+++ Rozdil 7/along_part7.c
@@ -93,7 +93,7 @@ void n_chars(char c, int n)
 */
 
 //-----------------------7.4------------------
-
+/*
 #include <iostream>
 
 long double probability(unsigned numbers, unsigned picks);
@@ -122,4 +122,113 @@ long double probability(unsigned numbers, unsigned picks)
     for(n = numbers, p = picks; p > 0;n--,p--)
         result = result *n/p;
     return result;
+}
+*/
+
+//-------------------7.7-----------------
+/*
+#include <iostream>
+
+const int Max = 5;
+
+int fill_array(double ar[], int limit);
+void show_array(const double ar[], int n);
+void revalue_array(double r, double ar[], int n);
+
+int main()
+{
+    using std::cin;
+    using std::cout;
+    using std::endl;
+
+    double properties[Max];
+    int size = fill_array(properties, Max);
+    show_array(properties, size);
+    if(size > 0 )
+    {
+        cout << "Enter ravaluation factor: ";
+        double factor;
+        while(!(cin >> factor))
+        {
+            cin.clear();
+            while(cin.get() != '\n')
+                continue;
+            cout << "Bad input; Please enter a number: ";
+        }
+        revalue_array(factor, properties,size);
+        show_array(properties, size);
+    }
+    cout << "Done" << endl;
+    cin.get();
+    cin.get();
+    return 0;
+}
+
+int fill_array(double ar[], int limit)
+{
+    using std::cin;
+    using std::cout;
+    using std::endl;
+    double temp;
+    int i;
+    for(i = 0; i < limit; i++)
+    {
+        cout << "Enter a value №" << (i+1) << ": ";
+        cin >> temp;
+        if(!cin)
+        {
+            cin.clear();
+            while (cin.get() != '\n')
+                continue;
+            cout <<"Bad input; input terminated." << endl;
+            break;
+        }
+        else if(temp < 0)
+            break;
+        ar[i] = temp;
+    }
+    return i;
+}
+
+void show_array(const double ar[], int n)
+{
+    for (int i = 0;i < n; i++)
+    {
+        std::cout << "Property №" << (i+1) << ": $";
+        std::cout << ar[i] << std::endl;
+    }
+}
+
+void revalue_array(double r, double ar[], int n)
+{
+    for(int i = 0; i < n; i++)
+        ar[i] *= r;
+}
+*/
+
+//--------------7.8------------------
+
+#include <iostream>
+
+const int ArSize = 8;
+int sum_arr (const int *begin, const int * end);
+int main()
+{
+    using namespace std;
+    int cookies[ArSize] = {1,2,4,8,16,32,64,128};
+    int sum = sum_arr(cookies, cookies+ArSize);
+    cout << "Total cookies eaten: " << sum << endl;
+    sum = sum_arr(cookies, cookies+3);
+    cout <<"First three eaters ate " << sum << " cookies." << endl;
+    sum = sum_arr(cookies+4, cookies+8);
+    cout << "Last fout eaters ate "<< sum << " cookies." << endl;
+    return 0;
+}
+int sum_arr(const int* begin, const int* end)
+{
+    const int* pt;
+    int total = 0;
+    for(pt = begin; pt != end; pt++)
+        total = total + *pt;
+    return total;
 }
